02d09137420895107bb8590a231dc0877c7c730a
nur hikmah <nur.hikmah190504@gmail.com>
2023-06-23 09:45:42 +0700

    cout << "Queue undeflow\n";    return;   }   cout << "\nThe elemnet delete from the queue  is: " << queues_array[FRONT] << "\n";

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -46,7 +46,11 @@ public:
 			REAR = -1;
 		}
 		else {
-			// jika elemen yang dihapus berada di posisi
+			// jika elemen yang dihapus berada di posisi terakhir array, kembali keawal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
 		}
 	}
 }; 
