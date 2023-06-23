721a717252f9f3e09b1704455accc8ea88aa7f1a
nur hikmah <nur.hikmah190504@gmail.com>
2023-06-23 12:25:02 +0700

    int main() {  Queues q;  char ch;
     while (true) {
      try {

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -67,15 +67,19 @@ public:
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
 				cout << queues_array[FRONT_position] << " ";
-				FRONT_position 0;
-
-				//interasi dari awal array hingga REAR
-				while (FRONT_position <= REAR_position) {
-					cout << queues_array[FRONT_position] << " ";
-					FRONT_position++;
-				}
-				cout << endl;
+				FRONT_position++;
 			}
+			cout << endl;
+		}
+		else {
+			// jika  FRONT _position > REAR_position, interasi dari FRON hingga akhir array
+			while (FRONT_position <= n - 1) {
+				cout << queues_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			FRONT_position = 0;
+
+			// interasi dari awal array hingga akhir 
 		}
 	}
 }; 
